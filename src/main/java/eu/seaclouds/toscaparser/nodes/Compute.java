cb16ae0fbf30977b1ad4b2ce54d314e6fa7ebac2
perezp <diego.perez@polimi.it>
2015-03-09 11:04:48 +0100

    FIX: another slight fix in imports

---
+++ src/main/java/eu/seaclouds/toscaparser/nodes/Compute.java
@@ -0,0 +1,44 @@
+package eu.seaclouds.toscaparser.nodes;
+
+public class Compute {
+
+	private String message="noStart";
+	
+	private int times=1;
+
+	public Compute(){}
+	public Compute(String message, int times) {
+		this.message = message;
+		this.times = times;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getTimes() {
+		return times;
+	}
+
+	public void setTimes(int times) {
+		this.times = times;
+	}
+	
+	@Override
+	public String toString(){
+		String output="";
+		for(int i=0; i<times; i++){
+			output+=message;
+		}
+		
+		return output;
+	}
+	
+
+	
+	
+}
